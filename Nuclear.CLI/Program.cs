a98e78ad2252ba6e144c16e54b7257e2a719de30
Alword <anzer987@yandex.ru>
2020-01-23 19:21:05 +0300

    feature test cli enviroment

---
+++ Nuclear.CLI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Transmission.API.RPC;
+using Transmission.API.RPC.Entity;
+
+namespace Nuclear.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Client("http://127.0.0.1:9091/transmission/rpc", "", "username", "password");
+
+            var sessionInfo = client.GetSessionInformation();
+            var allTorrents = client.TorrentGet(TorrentFields.ALL_FIELDS);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
